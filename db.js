b25790c28cdbe3cfc7ae3fc03aebb1f3a3d8b63d
Abubakar Goraya <muhammadabubakargoraya@gmail.com>
2026-09-03 15:44:36 +0500

    remove node modules

---
+++ db.js
@@ -0,0 +1,30 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+let dbPromise;
+
+export function openDb() {
+
+    if (!dbPromise) {
+
+        dbPromise = open({
+            filename: './db.sqlite',
+            driver: sqlite3.Database
+        }).then(async db => {
+
+            await db.exec(`
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    firstName TEXT,
+                    lastName TEXT,
+                    age INTEGER
+                )
+            `);
+
+            return db;
+        });
+
+    }
+
+    return dbPromise;
+}
